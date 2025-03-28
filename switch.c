7b4269a1d2b1a27154ac8ab3c8f13e37a8651012
samyukta-2 <samyuktar48@gmail.com>
2025-03-28 12:28:42 +0530

    case 1 switch code

---
+++ switch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    printf("select a number: ");
+    printf("1.cse");
+    printf("2.aiml");
+    printf("3.ece");
+    printf("4.mec");
+    printf("enter a number from (1-4): ");
+
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("B.tech Cse\n");
+            break;
