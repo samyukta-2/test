4a16f96ea86f59d545d5fd588d8e70773f7f34a4
samyukta-2 <samyuktar48@gmail.com>
2025-04-02 23:29:00 +0530

    sudoku solver commit 2

--- sudoku.py
+++ sudoku.py
@@ -14,3 +14,22 @@ def solve(board):
                         board[r][c] = 0
                 return False
     return True
+def print_board(board):
+    print("\n".join(" ".join(str(n) if n else '.' for n in row) for row in board))
+
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+solve(sudoku_board)
+print("Solved Sudoku:")
+print_board(sudoku_board)
+
