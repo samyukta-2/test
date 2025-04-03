e1fb4c583b74bdcb5a7428023bfd6ce3682391f4
samyukta-2 <samyuktar48@gmail.com>
2025-04-04 00:03:50 +0530

    minesweeper game using python (commit 2)

--- minesweeper.py
+++ minesweeper.py
@@ -6,3 +6,11 @@ def create_board(size, num_mines):
 
 def count_adjacent_mines(board, row, col, size):
     return sum(board[r][c] == 'M' for r in range(max(0, row-1), min(size, row+2)) for c in range(max(0, col-1), min(size, col+2)))
+def reveal(board, display, row, col, size):
+    if display[row][col] != '_': return
+    count = count_adjacent_mines(board, row, col, size)
+    display[row][col] = str(count) if count else ' '
+    if count == 0:
+        for r in range(max(0, row-1), min(size, row+2)):
+            for c in range(max(0, col-1), min(size, col+2)):
+                reveal(board, display, r, c, size)
