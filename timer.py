60126441c8e549461d227f642fe9411dafb163a3
samyukta-2 <samyuktar48@gmail.com>
2025-04-03 23:48:13 +0530

    i creater a timer using python

---
+++ timer.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = f'{mins:02}:{secs:02}'
+        print(timer, end='\r')
+        time.sleep(1)
+        seconds -= 1
+
+    print("Time's up!")
+
+
+countdown(30)
