f508007c40bed0df6fa1a464056c85d8e88e3c1f
samyukta-2 <samyuktar48@gmail.com>
2025-04-03 23:59:23 +0530

    binary search using python (commit 2)

--- binary.py
+++ binary.py
@@ -12,3 +12,11 @@ def binary_search(arr, target):
             right = mid - 1 
 
     return -1  
+arr = [1, 3, 5, 7, 9, 11, 13]
+target = 7
+result = binary_search(arr, target)
+
+if result != -1:
+    print(f"Element found at index {result}")
+else:
+    print("Element not found")
