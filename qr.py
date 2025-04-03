87fe9a4c6ef99c24b997b6dca66db38ce06553cf
samyukta-2 <samyuktar48@gmail.com>
2025-04-03 23:52:35 +0530

    i creater a qr encoder/decoder using python (commit 1)

---
+++ qr.py
@@ -0,0 +1,16 @@
+import qrcode
+import cv2
+
+def generate_qr(data, filename="qrcode.png"):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill="black", back_color="white")
+    img.save(filename)
+    print(f"QR Code saved as {filename}")
