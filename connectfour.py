9a8c12a4053fca11aef5c3f47ff8942dca85d2bc
samyukta-2 <samyuktar48@gmail.com>
2025-04-04 19:51:01 +0530

    simple connect four game using python (commit 3)

--- connectfour.py
+++ connectfour.py
@@ -11,3 +11,22 @@ def drop(col, piece):
         if board[r][col] == " ":
             board[r][col] = piece
             return r
+def check_win(r, c, piece):
+    dirs = [(1,0), (0,1), (1,1), (1,-1)]
+    for dr, dc in dirs:
+        count = 1
+        for i in range(1, 4):
+            nr, nc = r + dr*i, c + dc*i
+            if 0 <= nr < ROWS and 0 <= nc < COLS and board[nr][nc] == piece:
+                count += 1
+            else:
+                break
+        for i in range(1, 4):
+            nr, nc = r - dr*i, c - dc*i
+            if 0 <= nr < ROWS and 0 <= nc < COLS and board[nr][nc] == piece:
+                count += 1
+            else:
+                break
+        if count >= 4:
+            return True
+    return False
