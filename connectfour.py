2607e80021ba9ab1be10c489fc04909779b74d4e
samyukta-2 <samyuktar48@gmail.com>
2025-04-04 19:50:06 +0530

    simple connect four game using python (commit 2)

--- connectfour.py
+++ connectfour.py
@@ -6,3 +6,8 @@ def print_board():
     for row in board:
         print("|" + "|".join(row) + "|")
     print(" " + " ".join(str(i) for i in range(COLS)))
+def drop(col, piece):
+    for r in range(ROWS-1, -1, -1):
+        if board[r][col] == " ":
+            board[r][col] = piece
+            return r
