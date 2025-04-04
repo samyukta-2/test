db33b1a51bf6c612237f247cc0da48d33d2b8253
samyukta-2 <samyuktar48@gmail.com>
2025-04-04 19:42:17 +0530

    bank management system using python (commit 2)

--- bank.py
+++ bank.py
@@ -5,4 +5,11 @@ def create_account():
     name = input("Enter account holder name: ")
     accounts[acc_no] = {"name": name, "balance": 0}
     print("Account created successfully.")
-
+def view_account():
+    acc_no = input("Enter account number: ")
+    if acc_no in accounts:
+        acc = accounts[acc_no]
+        print("Name:", acc["name"])
+        print("Balance:", acc["balance"])
+    else:
+        print("Account not found.")
