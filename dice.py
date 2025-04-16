06b43ddb804cdad58e153fe036a6d3958d270a3e
samyukta-2 <samyuktar48@gmail.com>
2025-04-16 22:24:24 +0530

    dice rolling simulator using python

--- dice.py
+++ dice.py
@@ -2,3 +2,8 @@ import random
 
 def roll_dice():
     return random.randint(1, 6)
+while True:
+    input("Press Enter to roll the dice...")
+    result = roll_dice()
+    print(f"You rolled a {result} 🎲")
+
