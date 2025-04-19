003461c5374801a2cb14d90985025a5d0acad49f
samyukta-2 <samyuktar48@gmail.com>
2025-04-19 12:10:26 +0530

    attendance management system [main code]

---
+++ attendance.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.time.LocalDate;
+
+public class AttendanceApp extends JFrame {
+    private JTextField nameField;
+    private JButton markButton;
+
