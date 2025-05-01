546a4a93b90ee376a660bebc0f12003362feac29
samyukta-2 <samyuktar48@gmail.com>
2025-05-01 17:20:06 +0530

    created pong using python

--- pong.py
+++ pong.py
@@ -30,3 +30,23 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.2
 ball.dy = 0.2
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    if y < 250:
+        paddle_a.sety(y + 20)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    if y > -240:
+        paddle_a.sety(y - 20)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    if y < 250:
+        paddle_b.sety(y + 20)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    if y > -240:
+        paddle_b.sety(y - 20)
