6502154fcda9e5645966c198f433003c1a26425c
samyukta-2 <samyuktar48@gmail.com>
2025-05-01 17:25:14 +0530

    created pong using python

--- pong.py
+++ pong.py
@@ -78,4 +78,6 @@ while True:
     if (340 < ball.xcor() < 350) and (paddle_b.ycor() - 50 < ball.ycor() < paddle_b.ycor() + 50):
         ball.setx(340)
         ball.dx *= -1
-
+  if (-350 < ball.xcor() < -340) and (paddle_a.ycor() - 50 < ball.ycor() < paddle_a.ycor() + 50):
+        ball.setx(-340)
+        ball.dx *= -1
