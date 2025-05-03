a1dafb48254c6fd8a8f465134efb6a3904289c29
samyukta-2 <samyuktar48@gmail.com>
2025-05-03 11:14:50 +0530

    created a snakes game using python

--- snakegame.py
+++ snakegame.py
@@ -27,3 +27,11 @@ food.goto(0, 100)
 
 segments = []
 
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+
