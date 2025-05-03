11711ac2e84ea85f51b066ec4169cb6424feffc3
samyukta-2 <samyuktar48@gmail.com>
2025-05-03 11:14:12 +0530

    created a snakes game using python

--- snakegame.py
+++ snakegame.py
@@ -5,4 +5,25 @@ import random
 delay = 0.1
 score = 0
 high_score = 0
+win = turtle.Screen()
+win.title("Snake Game")
+win.bgcolor("black")
+win.setup(width=600, height=600)
+win.tracer(0)
+
+head = turtle.Turtle()
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+segments = []
 
