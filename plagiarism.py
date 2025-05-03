185bbccf3e9a05d80066863f17926d5a65c5b5bc
samyukta-2 <samyuktar48@gmail.com>
2025-05-03 11:06:00 +0530

    created a plagiarism checker using python

--- plagiarism.py
+++ plagiarism.py
@@ -3,3 +3,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def check_plagiarism(text1, text2):
     documents = [text1, text2]
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(documents)
+
+    similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
+    return similarity * 100
