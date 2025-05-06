0a325f0be9577fe484d0ab6fc300f56142e5e54f
samyukta-2 <samyuktar48@gmail.com>
2025-05-06 20:43:38 +0530

    created a cows and bulls game using python

--- cows.py
+++ cows.py
@@ -11,3 +11,8 @@ def calculate_cows_and_bulls(secret, guess):
     cows = sum(s == g for s, g in zip(secret, guess))
     bulls = sum(min(secret.count(d), guess.count(d)) for d in set(guess)) - cows
     return cows, bulls
+
+def play_cows_and_bulls():
+    print("Welcome to the Cows and Bulls Game!")
+    secret_number = generate_number()
+    attempts = 0
