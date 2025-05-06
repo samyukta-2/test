a2bde7359f5cdeb6b18b363311538dea3c03d645
samyukta-2 <samyuktar48@gmail.com>
2025-05-06 21:18:46 +0530

    created a higher lower game using python

--- higher.py
+++ higher.py
@@ -4,3 +4,5 @@ def higher_lower_game():
     print("Welcome to the Higher-Lower Game!")
     secret_number = random.randint(1, 100)  # The secret number is between 1 and 100
     attempts = 0
+    while True:
+        guess = input("Guess a number between 1 and 100 (or type 'exit' to quit): ")
